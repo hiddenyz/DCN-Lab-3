a1eefdcb7f83406a0d5687140d1d9990673e5041
hiddenyz <113217365+hiddenyz@users.noreply.github.com>
2023-03-06 23:54:35 -0500

    Update as.py

--- AS/as.py
+++ AS/as.py
@@ -1,1 +1,34 @@
+from socket import *
 
+dns_records = {}
+
+authoritative_socket = socket(AF_INET, SOCK_DGRAM)
+authoritative_socket.bind(('', 53533))
+
+while True:
+    message, client_address = authoritative_socket.recvfrom(2048)
+    message = message.decode()
+
+    print(message)
+
+    fields = message.strip().split('\n')
+    if len(fields) != 3:
+        print("Invalid DNS request")
+    else:
+        dns_type = fields[0].split('=')[1]
+        hostname = fields[1].split('=')[1]
+        ip = fields[2].split('=')[1]
+
+        if dns_type == 'A':
+            dns_records[hostname] = ip
+            response = b'Finished registration'
+        elif dns_type == 'Q':
+            if hostname in dns_records:
+                ip = dns_records[hostname]
+            else:
+                ip = '0.0.0.0'
+            response = f'TYPE=A\nNAME={hostname}\nVALUE={ip}\nTTL=10'.encode()
+        else:
+            print("Invalid DNS type")
+        
+        authoritative_socket.sendto(response, client_address)
