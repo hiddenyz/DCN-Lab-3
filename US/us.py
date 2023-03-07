97f6acd0e5983e4bd40ba9ef2878274dc56107a0
hiddenyz <113217365+hiddenyz@users.noreply.github.com>
2023-03-06 23:18:31 -0500

    Update us.py

--- US/us.py
+++ US/us.py
@@ -1,21 +1,45 @@
-FROM python:3.5
+from flask import Flask, abort, request
+from socket import *
 
-RUN apt-get update \
-  && apt-get clean \
-  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+app = Flask(__name__)
+
+@app.route('/')
+
+def initialize():
+    return "You should visit /fibonacci with several parameters to obtain the IP address"
 
 
-RUN groupadd -g 799 nyu && \
-    useradd -r -u 999 -g nyu nyu
+@app.route('/fibonacci', methods = ["GET"], endpoint="fibonacci")
+def us():
+    required_params = ["hostname", "fs_port", "number", "as_ip", "as_port"]
+    for param in required_params:
+        if param not in request.args:
+            abort(400)
 
-# Set up a working folder and install the pre-reqs
-WORKDIR /app
+    hostname = request.args.get("hostname")
+    fs_port = request.args.get("fs_port")
+    number = request.args.get("number")
+    as_ip = request.args.get("as_ip")
+    as_port = request.args.get("as_port")
 
-RUN pip install Flask
-RUN pip install requests
+    dns_message = "TYPE=A\n" + "NAME=" + hostname
+    user_socket = socket(AF_INET, SOCK_DGRAM)
+    address = (as_ip, int(as_port))
+    user_socket.sendto(dns_message.encode(), address)
+    response_message, authoritative_address = user_socket.recvfrom(2048)
+    response = response_message.decode().split('\n')
+    
+    ip = "0.0.0.0"
+    for line in response:
+        name, value = line.split('=')
+        if name == 'VALUE':
+            ip = value
+            break
 
-USER nyu
+        if ip == "0.0.0.0":
+            return "No IP address", 400
+        else:
+            return ip
 
-COPY --chown=nyu:nyu . .
-
-CMD [ "python", "./us.py" ]
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
